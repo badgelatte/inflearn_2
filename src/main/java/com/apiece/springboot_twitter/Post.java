083ed1a387749adb4b725e2128ec430a723507bb
badgelatte <saucy5939@gmail.com>
2026-06-05 18:05:04 +0900

    feat: Entity로 변경
    Entity로 변경

--- src/main/java/com/apiece/springboot_twitter/Post.java
+++ src/main/java/com/apiece/springboot_twitter/Post.java
@@ -1,17 +1,26 @@
 package com.apiece.springboot_twitter;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY = DB 컬럼 설정에 맞게 JPA가 따라간다
     private Long id;
+
     private String content;
+
     private LocalDateTime createdAt;
 
     public void updateContent(String content) {
