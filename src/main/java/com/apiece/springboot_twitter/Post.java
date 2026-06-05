d7cd33f3334099a79a71f900b5b36ceaf40c162d
badgelatte <saucy5939@gmail.com>
2026-06-05 16:50:42 +0900

    feat: content 수정을 위한 기능 추가
    content 수정을 위해 새로운 Post 객체를 만드는 기능 추가

--- src/main/java/com/apiece/springboot_twitter/Post.java
+++ src/main/java/com/apiece/springboot_twitter/Post.java
@@ -8,4 +8,9 @@ public record Post(
         String content,
         LocalDateTime createdAt
 ) {
+    // recored는 setter는 없으니 새로 만들어야한다
+    public Post updateContent(String content) {
+//        this.content = content; -> 이는 불가, content가 final 필드로 사용되기 때문
+        return new Post(this.id, content, this.createdAt);
+    }
 }
