19bebc73a21a8451aac8131bd335f72388cca5c7
badgelatte <saucy5939@gmail.com>
2026-06-11 23:36:57 +0900

    feat: RepositoryConfig 파일 작성
    RepositoryConfig 파일 작성

---
+++ src/main/java/com/apiece/springboot_twitter/repository/RepositoryConfig.java
@@ -0,0 +1,13 @@
+package com.apiece.springboot_twitter.repository;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RepositoryConfig {
+
+    @Bean // 원래는 (name = "postRepository")로 지정하나 없으면 메서드명을 따라간다
+    public PostRepository postRepository(JpaPostRepository jpaPostRepository) {
+        return jpaPostRepository;
+    }
+}
